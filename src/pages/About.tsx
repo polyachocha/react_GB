fa68c428d107183a6dd2aa134a1ea0b56cfb2764
Anna Polyakova <polyakova.as@mail.ru>
2022-10-02 13:45:06 +0300

    hw lesson-6

---
+++ src/pages/About.tsx
@@ -0,0 +1,30 @@
+import { FC } from 'react';
+import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
+import { toggleProfile } from '../store/profile/actions';
+import { ProfileState } from '../store/profile/reducer';
+
+export const About: FC = ({ visible, toggle }: any) => {
+  return (
+    <>
+      <h2>About page</h2>
+      <p>visible: </p>
+
+      <input type="checkbox" checked={visible} readOnly />
+      <button onClick={() => toggle()}>change visible</button>
+    </>
+  );
+};
+
+const mapStateToProps = (state: ProfileState) => ({
+  visible: state.visible,
+});
+
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  toggle: () => dispatch(toggleProfile()),
+});
+
+export const AboutWithConnect = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(About);
